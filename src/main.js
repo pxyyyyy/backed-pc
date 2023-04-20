356ab21e8c1f9a656109e3134d8f6beaf53e244e
潘心怡 <1611474513@qq.com>
2023-04-20 14:29:01 +0800

    引入组件

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 全局引用
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
+// 局部引用
+// import { Button } from 'element-ui';
+// import 'element-ui/lib/theme-chalk/index.css';
+// Vue.use(Button)
 
 Vue.config.productionTip = false
 
